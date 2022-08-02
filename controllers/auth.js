4b80d87f28a041e62a515cdb6983e7e0c3e47ce3
Ayilk <srangel.h92@gmail.com>
2022-08-02 13:12:34 -0500

    Generar JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const {response} = require('express');
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcryptjs');
+const {generarJWT} = require('../helpers/jwt');
 
 const crearUsuario = async(req, res = response) => {    
     
@@ -18,23 +19,30 @@ const crearUsuario = async(req, res = response) => {
                 msg: "El usuario ya existe con ese email"
             })
         }
+
+
         //Crear usuario con el modelo
         const dbUser = new Usuario(req.body);
+
 
         //Hashear la contraseña
         const salt = bcrypt.genSaltSync();//10 vueltas
         dbUser.password = bcrypt.hashSync( password, salt )
 
+
         //Generar el JST
+        const token = await generarJWT(dbUser.id, name);
+
 
         //Crear usuario de DB
         await dbUser.save();
+
 
         //Generar respuesta exitosa
         return res.status(201).json({
             ok:true,
             uid: dbUser.id,
-            name
+            name, token
         })
 
 
