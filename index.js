c7c456f9f7be5ed65e28eb63c21bd028915bc879
Ayilk <srangel.h92@gmail.com>
2022-08-05 17:42:24 -0500

    Desplegando Angular en Node

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -25,6 +26,11 @@ app.use(express.json());
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 
+//Manejar otras rutas
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
+
 //GET
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -34,7 +40,7 @@ app.get('/', (req, res) => {
     })
     
 })
-
+ 
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
